bd8605c8b4aa2cfc1c2ecfb7d6adaa5db71ac6b4
Farah Sallam <farahsallam@student.olympic.edu>
2024-04-28 16:19:55 -0700

    JSON loop

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ fav_games = JSON.parse(jsonString);
 const table = document.getElementById("games");
 
 // Loop through all of the items in the JSON and extract the data and put it into a table
+for(i in fav_games.games)
+{
+    const row = document.createElement("tr");
+
+    const cell1 = document.createElement("td");
+    const cell2 = document.createElement("td");
+    const cell3 = document.createElement("td");
+
+    cell1.textContent = fav_games.games[i].title;
+    cell1.classList.add("left");
+    row.appendChild(cell1);
+
+    cell2.textContent = fav_games.games[i].description;
+    row.appendChild(cell2);
+
+    cell3.textContent = "$" + fav_games.games[i].price;
+    row.appendChild(cell3);
+
+    table.appendChild(row);
+
+}
